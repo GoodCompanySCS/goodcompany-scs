985ac5fa6a41923ec24f7f6f8718f554a16acd61
GoodCompanySCS <perrasbydesign@gmail.com>
2026-05-12 11:01:44 -0400

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -71,7 +71,7 @@ export default function About() {
               </ul>
             </div>
 
-            <div>
+            <div className="mb-12">>
               <h2 style={{ color: 'var(--sage)' }} className="mb-6">Service Area</h2>
               <p className="text-lg mb-4">
                 Good Company currently serves seniors in:
