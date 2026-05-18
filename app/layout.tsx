c5a79dc2bf344059b49cfe8f177a1996075400b0
GoodCompanySCS <perrasbydesign@gmail.com>
2026-05-18 19:32:09 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Good Company | Senior Concierge Services",
@@ -27,11 +28,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-     <body>
-       <Header />
-       <main>{children}</main>
-      <Footer />
-     </body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LSC35890EF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LSC35890EF');
+          `}
+        </Script>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
