97eadf01dd5b6bdb2858e0919257bd90c572af05
GoodCompanySCS <perrasbydesign@gmail.com>
2026-05-18 19:25:47 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSC35890EF"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LSC35890EF');
+        `}} />
+      </head>
       <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </body>
     </html>
   );
 }
