e9ea8f01b7e457bdebcb53efcc799c43f1d78655
GoodCompanySCS <perrasbydesign@gmail.com>
2026-06-19 14:16:32 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,7 +152,7 @@ function Footer() {
               style={{
                 backgroundColor: "#F7F1E8",
                 borderRadius: "8px",
-                padding: 12px",
+                padding: "4px, 12px",
                 display: "inline-block",
               }}
             >
